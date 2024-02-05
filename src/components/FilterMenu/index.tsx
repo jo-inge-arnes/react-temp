97911124200fb998e7e9d72fd132eab010a9effd
Jo Inge Arnes <jo.i.arnes@uit.no>
2024-02-05 16:38:50 +0100

    wiring up FilterMenu handleSelectionChanged callback with filter section child components

--- src/components/FilterMenu/index.tsx
+++ src/components/FilterMenu/index.tsx
@@ -1,15 +1,40 @@
-import { useState, PropsWithChildren } from 'react';
+import { ReactElement, PropsWithChildren, cloneElement } from 'react';
 import Stack from '@mui/material/Container';
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
 
+export type FilterMenuSection = PropsWithChildren<{
+    handleSelectionChanged: () => void;
+    children: ReactElement<FilterMenuSectionProps> | ReactElement<FilterMenuSectionProps>[];
+}>;
 
-type FilterMenuProps = {
+export type FilterMenuSectionProps = PropsWithChildren<{
+    filterSectionId: string | number;
+    filterSectionTitle: string;
+    handleSelectionChanged?: () => void;
+}>;
 
-};
+const FilterMenu = ({ handleSelectionChanged, children }: FilterMenuSection) => {
+    const buildSection = (elmt: ReactElement<FilterMenuSectionProps>) => {
+        const { filterSectionId, filterSectionTitle } = elmt.props;
+        elmt = cloneElement(elmt, { handleSelectionChanged: handleSelectionChanged });
 
-const FilterMenu = (props: PropsWithChildren<FilterMenuProps>) => {
+        return (
+            <Accordion key={`filtermenusection-${filterSectionId}`}>
+                <AccordionSummary>
+                    {filterSectionTitle}
+                </AccordionSummary>
+                <AccordionDetails>
+                    {elmt}
+                </AccordionDetails>
+            </Accordion>
+        );
+    };
+
+    const sections = Array.isArray(children) ? children.map(buildSection) : buildSection(children);
+
     return (
         <Stack>
-            {props.children}
+            {sections}
         </Stack>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-import FilterMenu from "@/components/FilterMenu";
-import { CheckBox } from "@mui/icons-material";
+import FilterMenu, { FilterMenuSectionProps } from "@/components/FilterMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export const MyFilterSection = (props: FilterMenuSectionProps) => {
+  return (
+    <button onClick={props.handleSelectionChanged}>klikk mæ</button>
+  );
+}
 
+export default function Home() {
   return (
     <>
       <Head>
@@ -20,95 +22,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
         <div className={styles.center}>
-          <FilterMenu>
-            Hei!
+          <FilterMenu handleSelectionChanged={() => console.log("halloisen!")}>
+            <MyFilterSection
+              filterSectionId="1"
+              filterSectionTitle="Filter 1">
+
+            </MyFilterSection>
           </FilterMenu>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        </div >
+      </main >
     </>
   );
 }
