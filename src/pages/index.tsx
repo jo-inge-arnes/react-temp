f0cae2835248f1941a7f9627e9b81639e4d5e154
Jo Inge Arnes <jo.i.arnes@uit.no>
2024-02-16 14:46:14 +0100

    added tests for FilterMenu initialState()

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div>Run <code>yarn storybook</code> to see the components in action.</div>
+        <div>
+          Run <code>yarn storybook</code> to see the components in action.
+        </div>
       </main>
     </>
   );
